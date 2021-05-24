6530b0362e8786d4ad5c05b5fe356b1615813b2c
Timdnb <timdnblanken@gmail.com>
2021-05-24 21:37:28 +0200

    Add explanation orbital parameters and change values

--- main/solar_system.py
+++ main/solar_system.py
@@ -12,12 +12,14 @@ from plot_orbits import plot_orbits
 # Astronomical unit value
 AU = 149597871  # km
 
+# List containing all necessary orbital parameters
+# Semi-major axis is in km
 all_planets = [
-    # a        e         inc periapsis ascending anomaly name
-    (57909050, 0.205630, 7.02, 29.124, 48.331, 0, "Mercury", "sandybrown"),
-    (108208000, 0.006772, 3.39, 54.884, 76.68, 0, "Venus", "gold"),
-    (149696000, 0.0167086, 0, 114.207, -11.26, 0, "Earth", "mediumspringgreen"),
-    (227939200, 0.0934, 1.85, 286.50, 49.558, 0, "Mars", "tomato"),
+    # a        e         inc    periapsis ascending anomaly name color
+    (57909050, 0.205630, 7.005, 29.124, 48.331, 0, "Mercury", "sandybrown"),
+    (108209000, 0.006772, 3.39, 54.884, 76.68, 0, "Venus", "gold"),
+    (149596000, 0.0167086, 0, 114.207, -11.26, 0, "Earth", "mediumspringgreen"),
+    (227939200, 0.0935, 1.85, 286.50, 49.558, 0, "Mars", "tomato"),
     (5.2044 * AU, 0.0489, 1.31, 273.867, 100.464, 0, "Jupiter", "moccasin"),
     (9.5826 * AU, 0.0565, 2.49, 339.392, 113.665, 0, "Saturn", "navajowhite"),
     (19.2185 * AU, 0.046381, 0.77, 96.998857, 74.006, 0, "Uranus", "powderblue"),
