c93d0e71da9d23c6f00e8293302ed2152bb0de84
Timdnb <timdnblanken@gmail.com>
2021-05-24 21:53:10 +0200

    Add explanatory comments

--- main/random_planetary_system.py
+++ main/random_planetary_system.py
@@ -8,6 +8,9 @@ from generate_random_planet_names import generate_random_planet_names
 from plot_orbits import plot_orbits
 
 def random_color():
+    """
+    Generate random rgb color
+    """
     rgb=[random.randint(0, 255)/255, random.randint(0, 255)/255, random.randint(0, 255)/255]
     return tuple(rgb)
 
@@ -18,12 +21,18 @@ def random_color():
 # Astronomical unit value
 AU = 149597871  # km
 
+# Generate random number which will be the amount of planets (3 min, 20 max)
 amount_of_planets = random.randint(3,20)
 
+# Start with empty list
 all_planets = []
+
+# Generate as many random names as planets
 planet_names = generate_random_planet_names(amount_of_planets)
 
+# Fill all_planets list with all necessary values to plot the orbits
 for i in list(range(amount_of_planets)):
+    #        semi-major axis              eccentricity         inclination             argument of pericenter  long. of asc. node     t_p name            color
     planet = (random.uniform(0, 30 * AU), random.uniform(0,1), random.uniform(0, 180), random.uniform(0, 360), random.uniform(0, 360), 0, planet_names[i], random_color())
     all_planets.append(planet)
 
